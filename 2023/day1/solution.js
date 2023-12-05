28ecdce974d2c9470dffb6d776a8f5ecf8433f5a
Sobition <s.aminnejad2013@gmail.com>
2023-12-05 18:23:08 +0100

    Day 1

---
+++ 2023/day1/solution.js
@@ -0,0 +1,40 @@
+const { readFile, timeIt, sumArray } = require("../../util");
+const part1 = () => {
+  const lines = readFile("./input.txt").map((line) => line);
+  const tuples = lines.map((line) => {
+    const numbers = line.split("").filter((char) => !isNaN(char)); // Check if the character is a number
+
+    return Number(numbers[0] + numbers[numbers.length - 1]);
+  });
+  console.log("part1: ", sumArray(tuples));
+};
+timeIt(part1);
+
+const part2 = () => {
+  const lines = readFile("./input.txt").map((line) => line);
+
+  const numberMap = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  const tuples = lines.map((row) => {
+    Object.keys(numberMap).forEach((key) => {
+      if (row.includes(key))
+        row = row.replaceAll(key, `${key}${numberMap[key]}${key}`);
+    });
+    const firstDigit = row.match(/\d/)[0];
+    const lastDigit = row.match(/(\d)[a-z]*$/)[1];
+    return parseInt(`${firstDigit}${lastDigit}`);
+  });
+
+  console.log("part2: ", sumArray(tuples));
+};
+timeIt(part2);
