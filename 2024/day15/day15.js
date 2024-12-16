b7bc71512cb3fb378a525ac2eb5dcc6b0915fa6d
Sobition <s.aminnejad2013@gmail.com>
2024-12-16 07:11:08 +0100

    2024 day 15

--- 2024/day15/day15.js
+++ 2024/day15/day15.js
@@ -32,14 +32,6 @@ function getFinalBoxDistances(grid, movements) {
     );
   }
 
-  // Helper function to print the grid
-  function printGrid() {
-    console.log(grid.map((row) => row.join('')).join('\n'));
-    console.log('');
-  }
-
-  // Direction mappings
-
   // Process each movement
   for (const move of movements) {
     const { dr, dc } = directions[move];
@@ -92,7 +84,6 @@ function getFinalBoxDistances(grid, movements) {
         robotPos = { row: newRow, col: newCol };
       }
     }
-    printGrid(); // Print the grid after each move
   }
 
   // Calculate distances of all boxes from the top and left edges
@@ -116,6 +107,8 @@ const solvePart1 = (input) => {
     .map((row) => row.split(''));
   const moves = movesInput.replace(/\n/g, '').split('');
 
+  console.log(moves);
+  console.log(grid);
   const finalDistances = getFinalBoxDistances(grid, moves);
   const sum = finalDistances.reduce(
     (acc, box) => acc + box.top * 100 + box.left,
@@ -125,15 +118,135 @@ const solvePart1 = (input) => {
   return sum;
 };
 
-const solvePart2 = (input) => {
-  const [gridInput, movesInput] = input;
-  const grid = gridInput
-    .trim()
-    .split('\n')
-    .map((row) => row.split(''));
-  const moves = movesInput.replace(/\n/g, '').split('');
+///////////////////////
+function getFinalBoxDistancesP2(input) {
+  let grid = input[0]
+    .replace(/\#/g, '##')
+    .replace(/\O/g, '[]')
+    .replace(/\./g, '..')
+    .replace(/\@/g, '@.')
+    .split(/\r?\n/)
+    .map((x) => x.split(''));
+  let moves = input[1].replace(/[\r\n]/g, '').split('');
 
-  return 'Solution for Part 2';
+  let x = null,
+    y = null;
+  for (let i = 0; x === null && i < grid.length; ++i) {
+    for (let j = 0; j < grid[0].length; ++j) {
+      if (grid[i][j] === '@') {
+        x = j;
+        y = i;
+        break;
+      }
+    }
+  }
+
+  for (let move of moves) {
+    const { dr, dc } = directions[move];
+
+    let newX = x + dc;
+    let newY = y + dr;
+
+    if (grid[newY][newX] === '.') {
+      grid[newY][newX] = '@';
+      grid[y][x] = '.';
+      y = newY;
+      x = newX;
+      continue;
+    }
+    if (grid[newY][newX] === '#') {
+      continue;
+    }
+
+    let [boxL_x, boxL_y, boxR_x, boxR_y] = getBoxBounds(newY, newX);
+
+    if (shiftBox(true, boxL_y, boxL_x, boxR_y, boxR_x, dr, dc)) {
+      shiftBox(false, boxL_y, boxL_x, boxR_y, boxR_x, dr, dc);
+      grid[y][x] = '.';
+      grid[newY][newX] = '@';
+      y = newY;
+      x = newX;
+    }
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < grid.length; ++i) {
+    for (let j = 0; j < grid[0].length; ++j) {
+      if (grid[i][j] === '[') {
+        result += i * 100 + j;
+      }
+    }
+  }
+
+  return result;
+
+  function shiftBox(dryRun, boxL_y, boxL_x, boxR_y, boxR_x, dr, dc) {
+    let q = [
+      [boxL_y, boxL_x, '['],
+      [boxR_y, boxR_x, ']'],
+    ];
+    let seen = new Set([toDp(boxL_y, boxL_x), toDp(boxR_y, boxR_x)]);
+
+    if (!dryRun) {
+      grid[boxL_y][boxL_x] = '.';
+      grid[boxR_y][boxR_x] = '.';
+    }
+
+    while (q.length) {
+      let [moveY, moveX, val] = q.pop();
+
+      let newY = moveY + dr;
+      let newX = moveX + dc;
+      if (grid[newY][newX] === '.') {
+      } else if (grid[newY][newX] === '#') {
+        return false;
+      } else {
+        let [box2L_x, box2L_y, box2R_x, box2R_y] = getBoxBounds(newY, newX);
+
+        if (!seen.has(toDp(box2L_y, box2L_x))) {
+          seen.add(toDp(box2L_y, box2L_x));
+          seen.add(toDp(box2R_y, box2R_x));
+
+          q.push([box2L_y, box2L_x, '['], [box2R_y, box2R_x, ']']);
+
+          if (!dryRun) {
+            grid[box2L_y][box2L_x] = '.';
+            grid[box2R_y][box2R_x] = '.';
+          }
+        }
+      }
+
+      if (!dryRun) {
+        grid[newY][newX] = val;
+      }
+    }
+    return true;
+  }
+
+  function toDp(x, y) {
+    return y * grid[0].length + x;
+  }
+
+  function getBoxBounds(y, x) {
+    let box2L_x, box2L_y, box2R_x, box2R_y;
+    if (grid[y][x] === '[') {
+      box2L_x = x;
+      box2L_y = y;
+    } else {
+      box2L_x = x - 1;
+      box2L_y = y;
+    }
+    box2R_x = box2L_x + 1;
+    box2R_y = box2L_y;
+
+    return [box2L_x, box2L_y, box2R_x, box2R_y];
+  }
+}
+
+const solvePart2 = (input) => {
+  const result = getFinalBoxDistancesP2(input);
+  return result;
 };
 
 // Main execution
@@ -141,7 +254,7 @@ const main = () => {
   // Change `test.txt` to `input.txt` for full input
   const input = readFileSplitByEmptyLine('input.txt');
 
-  // console.log('Part 1:', timeIt(solvePart1, input));
+  console.log('Part 1:', timeIt(solvePart1, input));
   console.log('Part 2:', timeIt(solvePart2, input));
 };
 
