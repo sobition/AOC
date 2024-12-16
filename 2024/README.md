58ffd9cf9931aa4446a24e03115c3c13eefe8a4b
Sobition <s.aminnejad2013@gmail.com>
2024-12-16 08:51:21 +0100

    2024 day 16

--- 2024/README.md
+++ 2024/README.md
@@ -39,3 +39,5 @@ My solutions to puzzles at [adventofcode.com](https://adventofcode.com/2024).
 | [Day 12](https://adventofcode.com/2024/day/12) |   ⭐   |   ⭐   |
 | [Day 13](https://adventofcode.com/2024/day/13) |   ⭐   |   ⭐   |
 | [Day 14](https://adventofcode.com/2024/day/14) |   ⭐   |   ⭐   |
+| [Day 15](https://adventofcode.com/2024/day/15) |   ⭐   |   ⭐   |
+| [Day 16](https://adventofcode.com/2024/day/16) |   ⭐   |   ⭐   |
--- 2024/day16/day16.test.js
+++ 2024/day16/day16.test.js
@@ -1,18 +1,17 @@
-const path = require("path");
-const { solvePart1, solvePart2 } = require("./day16");
-const { readFile } = require("../../utils/utils");
+const path = require('path');
+const { solvePart1, solvePart2 } = require('./day16');
+const { readFile } = require('../../utils/utils');
 
 const input = readFile(path.resolve(__dirname, 'test.txt'));
 
-describe("Day 16 Solutions", () => {
-
-  test("solvePart1 should return the expected output for Part 1", () => {
+describe('Day 16 Solutions', () => {
+  test('solvePart1 should return the expected output for Part 1', () => {
     const result = solvePart1(input);
-    expect(result).toBe('Expected Part 1 Result'); 
+    expect(result).toBe(143580);
   });
 
-  test("solvePart2 should return the expected output for Part 2", () => {
+  test('solvePart2 should return the expected output for Part 2', () => {
     const result = solvePart2(input);
-    expect(result).toBe('Expected Part 2 Result'); 
+    expect(result).toBe(645);
   });
 });
