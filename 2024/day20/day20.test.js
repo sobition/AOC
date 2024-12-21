ce57734d4a9e6fdb9cc9fdedb18eb97ef5f919f6
Sobition <s.aminnejad2013@gmail.com>
2024-12-21 16:05:06 +0100

    fix tests

--- 2024/day20/day20.test.js
+++ 2024/day20/day20.test.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { solvePart1, solvePart2 } = require('./day20');
 const { readFile } = require('../../utils/utils');
 
-const input = readFile(path.resolve(__dirname, 'test.txt'));
+const input = readFile(path.resolve(__dirname, 'input.txt'));
 
 describe('Day 20 Solutions', () => {
   test('solvePart1 should return the expected output for Part 1', () => {
